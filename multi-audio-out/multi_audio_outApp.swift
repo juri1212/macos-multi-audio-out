7201b8f8b5d887ac65e3ed6fa2a2f6ddb990b008
juri1212 <juri.beforth@gmx.de>
2025-12-01 08:35:31 +0100

    Initial Commit

---
+++ multi-audio-out/multi_audio_outApp.swift
@@ -0,0 +1,17 @@
+//
+//  multi_audio_outApp.swift
+//  multi-audio-out
+//
+//  Created by Juri Beforth on 01.12.25.
+//
+
+import SwiftUI
+
+@main
+struct multi_audio_outApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
